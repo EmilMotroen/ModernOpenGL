c463633dbcb7038d0d879493402abd3efe45f930
Emil Motrøen <e.motroen@gmail.com>
2024-06-24 01:04:18 +0200

    Create window with blue background

--- ModernOpenGL/src/ModernOpenGL.cpp
+++ ModernOpenGL/src/ModernOpenGL.cpp
@@ -5,8 +5,46 @@
 #include <iostream>
 
 
+const char* APP_TITLE = "Introduction to Modern OpenGL";
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
-int main()
-{
-    
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "GLFW failed to initialize..." << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* pWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, APP_TITLE, NULL, NULL);
+    if (pWindow == NULL) {
+        std::cerr << "Failed to create GLFW window..." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(pWindow);
+
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "GLEW failed to initialize..." << std::endl;
+        return -1;
+    }
+
+    /* Main loop */
+    while (!glfwWindowShouldClose(pWindow)) {
+        glfwPollEvents();
+
+        glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(pWindow);
+    }
+
+    glfwTerminate();
 }
