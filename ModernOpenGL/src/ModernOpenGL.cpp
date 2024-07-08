ab9611bc76b323c4e8753d8f2b2bae785041a09e
Emil Motrøen <e.motroen@gmail.com>
2024-07-08 20:15:07 +0200

    Add GLM library

--- ModernOpenGL/src/ModernOpenGL.cpp
+++ ModernOpenGL/src/ModernOpenGL.cpp
@@ -69,6 +69,12 @@ int main() {
 
         shaderProgram.use();
 
+        GLfloat time = glfwGetTime();
+        GLfloat blueColor = (sin(time) / 2) + 0.5f;
+
+
+        shaderProgram.setUniform("vertColor", glm::vec4(0.0f, 0.0f, blueColor, 1.0f));
+
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
